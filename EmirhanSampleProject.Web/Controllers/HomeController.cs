48098ccea8085c0a4e64a8a29db211c42b34458e
emirhanselcuk <selcukemirhan88@gmail.com>
2024-11-05 16:41:54 +0300

    Tekrar degisiklik yapıldı

--- EmirhanSampleProject.Web/Controllers/HomeController.cs
+++ EmirhanSampleProject.Web/Controllers/HomeController.cs
@@ -115,8 +115,9 @@ namespace EmirhanSampleProject.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }); //view donduruldu
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }); //view donduruldu donduruldu2
         }
     }
     //degisiklik
+    //degisiklik2
 }
